18ad4fd937da279d6071edf35a0137ce54241cdb
Tetricz <DavidLeeDaniels@protonmail.com>
2021-12-13 17:26:43 -0600

    quick fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if ! test -f "./JMusic*"; then
     echo "Jarfile not found"
     echo "Downloading JMusic jarfile"
     curl -L $(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest | grep -i 'browser_download_url.*\.jar' | sed 's/.*\(http.*\)"/\1/') \
-    -o /$(echo $(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest | grep -i 'browser_download_url.*\.jar' | sed 's/.*\(http.*\)"/\1/') | sed 's/.*\/\([^\/]*\)/\1/')
+    -o /jmusic-bot/$(echo $(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest | grep -i 'browser_download_url.*\.jar' | sed 's/.*\(http.*\)"/\1/') | sed 's/.*\/\([^\/]*\)/\1/')
 fi
 echo "Starting server"
 java -Dnogui=true -jar $(ls JMusic*)
