608f1dbd83630a979006fb36c7830c37a0a4f83f
Tetricz <DavidLeeDaniels@protonmail.com>
2021-12-13 17:10:15 -0600

    add backup download

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,11 @@ cd /jmusic-bot
 if ! test -f "./config.txt"; then
     envsubst < "/config.txt.sample" > "./config.txt"
 fi
+if ! test -f "$(ls JMusic*)"; then
+    echo "Jarfile not found"
+    echo "Downloading JMusic jarfile"
+    curl -L $(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest | grep -i 'browser_download_url.*\.jar' | sed 's/.*\(http.*\)"/\1/') \
+    -o /$(echo $(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest | grep -i 'browser_download_url.*\.jar' | sed 's/.*\(http.*\)"/\1/') | sed 's/.*\/\([^\/]*\)/\1/')
+fi
 echo "Starting server"
 java -Dnogui=true -jar $(ls JMusic*)
