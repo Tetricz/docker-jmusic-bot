ef8d425e11fa9b3d0160ffebaf5ebc96f5573f5f
Tetricz <DavidLeeDaniels@protonmail.com>
2022-04-02 09:10:24 -0500

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd /jmusic-bot
-if ! test -f "./config.txt"; then
+if [ ! -e ./config.txt ]; then
     envsubst < "/config.txt.sample" > "./config.txt"
 fi
-if ! test -f "./JMusic*"; then
+if [ ! -e ./JMusic* ]; then
     echo "Jarfile not found"
     echo "Downloading JMusic jarfile"
     curl -L $(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest | grep -i 'browser_download_url.*\.jar' | sed 's/.*\(http.*\)"/\1/') \
